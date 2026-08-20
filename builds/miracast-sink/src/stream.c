ff344606ca1b0f4eaf96770ce5a7f6d18dc76e2e
kunshakolime <kunshakolime@gmail.com>
2026-08-20 19:45:24 +0100

    fix: tsdemux dynamic pads cause black screen, add safe local test
    - stream.c: Use pad-added callback for tsdemux instead of static linking.
      tsdemux creates source pads at runtime, so gst_element_link_many
      silently failed past it, producing a black screen.
    - test/test-local.sh: Interactive mock test that stops real miracle-wifid,
      runs mock source, and lets you watch the sink app connect.

--- builds/miracast-sink/src/stream.c
+++ builds/miracast-sink/src/stream.c
@@ -12,6 +12,11 @@ struct _MskStream
 
   GstElement *pipeline;
   GstElement *udpsrc;
+  GstElement *tsdemux;
+  GstElement *queue;
+  GstElement *h264parse;
+  GstElement *decoder;
+  GstElement *videoconvert;
   GstElement *sink;
   GdkPaintable *paintable;
 };
@@ -37,6 +42,38 @@ on_bus_eos (GstBus *bus, GstMessage *msg, gpointer user_data)
   g_debug ("GStreamer EOS");
 }
 
+static void
+on_tsdemux_pad_added (GstElement *src, GstPad *new_pad, gpointer user_data)
+{
+  MskStream *self = MSK_STREAM (user_data);
+  GstPad *sink_pad = NULL;
+  GstCaps *caps = NULL;
+  GstStructure *s = NULL;
+  const gchar *media_type = NULL;
+
+  caps = gst_pad_get_current_caps (new_pad);
+  if (!caps)
+    caps = gst_pad_query_caps (new_pad, NULL);
+  s = gst_caps_get_structure (caps, 0);
+  media_type = gst_structure_get_name (s);
+
+  if (g_str_has_prefix (media_type, "video/"))
+    {
+      sink_pad = gst_element_get_static_pad (self->queue, "sink");
+      if (!gst_pad_is_linked (sink_pad))
+        {
+          GstPadLinkReturn ret = gst_pad_link (new_pad, sink_pad);
+          if (GST_PAD_LINK_FAILED (ret))
+            g_warning ("Failed to link tsdemux video pad: %d", ret);
+          else
+            g_debug ("Linked tsdemux video pad to queue");
+        }
+      gst_object_unref (sink_pad);
+    }
+
+  gst_caps_unref (caps);
+}
+
 void
 msk_stream_start (MskStream *self)
 {
@@ -58,6 +95,10 @@ msk_stream_stop (MskStream *self)
 
   if (self->udpsrc)
     g_object_set (self->udpsrc, "port", self->port, NULL);
+
+  /* release dynamic pads on tsdemux so a restart works */
+  if (self->tsdemux)
+    gst_element_release_request_pads (self->tsdemux);
 }
 
 GdkPaintable *
@@ -98,28 +139,25 @@ MskStream *
 msk_stream_new (guint16 port)
 {
   MskStream *self;
-  GstElement *rtpjitterbuffer, *depay, *tsdemux, *queue;
-  GstElement *h264parse, *decoder, *videoconvert, *sink;
   GstBus *bus;
 
   self = g_object_new (MSK_TYPE_STREAM, NULL);
   self->port = port ? port : 7236;
 
-  self->pipeline = gst_pipeline_new ("miracast-sink");
-  self->udpsrc = gst_element_factory_make ("udpsrc", "udpsrc");
-  rtpjitterbuffer = gst_element_factory_make ("rtpjitterbuffer", NULL);
-  depay = gst_element_factory_make ("rtpmp2tdepay", NULL);
-  tsdemux = gst_element_factory_make ("tsdemux", NULL);
-  queue = gst_element_factory_make ("queue", NULL);
-  h264parse = gst_element_factory_make ("h264parse", NULL);
-  decoder = gst_element_factory_make ("avdec_h264", NULL);
-  videoconvert = gst_element_factory_make ("videoconvert", NULL);
-  sink = gst_element_factory_make ("gtk4paintablesink", "gtk4sink");
+  self->pipeline      = gst_pipeline_new ("miracast-sink");
+  self->udpsrc        = gst_element_factory_make ("udpsrc", "udpsrc");
+  GstElement *jitbuf  = gst_element_factory_make ("rtpjitterbuffer", NULL);
+  GstElement *depay   = gst_element_factory_make ("rtpmp2tdepay", NULL);
+  self->tsdemux       = gst_element_factory_make ("tsdemux", NULL);
+  self->queue         = gst_element_factory_make ("queue", NULL);
+  self->h264parse     = gst_element_factory_make ("h264parse", NULL);
+  self->decoder       = gst_element_factory_make ("avdec_h264", NULL);
+  self->videoconvert  = gst_element_factory_make ("videoconvert", NULL);
+  self->sink          = gst_element_factory_make ("gtk4paintablesink", "gtk4sink");
 
-  g_assert (self->udpsrc && rtpjitterbuffer && depay && tsdemux &&
-            queue && h264parse && decoder && videoconvert && sink);
-
-  self->sink = sink;
+  g_assert (self->udpsrc && jitbuf && depay && self->tsdemux &&
+            self->queue && self->h264parse && self->decoder &&
+            self->videoconvert && self->sink);
 
   g_object_set (self->udpsrc,
                 "port", self->port,
@@ -128,12 +166,20 @@ msk_stream_new (guint16 port)
                 NULL);
 
   gst_bin_add_many (GST_BIN (self->pipeline),
-                    self->udpsrc, rtpjitterbuffer, depay, tsdemux, queue,
-                    h264parse, decoder, videoconvert, sink, NULL);
+                    self->udpsrc, jitbuf, depay, self->tsdemux,
+                    self->queue, self->h264parse, self->decoder,
+                    self->videoconvert, self->sink, NULL);
 
-  gst_element_link_many (self->udpsrc, rtpjitterbuffer, depay, tsdemux,
-                         queue, h264parse, decoder, videoconvert, sink,
-                         NULL);
+  /* link statically up to tsdemux (its src pads are dynamic) */
+  gst_element_link_many (self->udpsrc, jitbuf, depay, self->tsdemux, NULL);
+
+  /* link downstream of tsdemux statically (queue -> h264parse -> decode -> convert -> sink) */
+  gst_element_link_many (self->queue, self->h264parse, self->decoder,
+                         self->videoconvert, self->sink, NULL);
+
+  /* connect dynamic pads from tsdemux */
+  g_signal_connect (self->tsdemux, "pad-added",
+                    G_CALLBACK (on_tsdemux_pad_added), self);
 
   bus = gst_element_get_bus (self->pipeline);
   gst_bus_add_signal_watch (bus);
@@ -141,7 +187,7 @@ msk_stream_new (guint16 port)
   g_signal_connect (bus, "message::eos", G_CALLBACK (on_bus_eos), self);
   gst_object_unref (bus);
 
-  g_object_get (sink, "paintable", &self->paintable, NULL);
+  g_object_get (self->sink, "paintable", &self->paintable, NULL);
 
   return self;
 }
